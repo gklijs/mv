319b3be5ef7567a4971ed4ba11227fc76519fc3f
Gerard Klijs <g.klijs@gmail.com>
2018-05-08 08:12:14 +0200

    fix login

--- src/clj/m_venue/authentication.clj
+++ src/clj/m_venue/authentication.clj
@@ -26,25 +26,23 @@
       {:status 303 :headers {"Location" "/login"}})
     {:status 303 :headers {"Location" "/login"}}))
 
-(def login-structure
-  [:div#main-content.tile.is-9.is-vertical
-   [:div.tile.is-parent
-    [:div.content.notification.tile.is-child.is-primary
-     [:p.title "Log in om content aan te passen"]
-     [:div.input-group.panel-body
-      [:form.form-signin {:action "/login" :method "POST"}
-       (anti-forgery-field)
-       [:div.field [:p.control.has-icons-left
-                    [:input.input {:placeholder "User ID" :type :text :name :uid}]
-                    [:span.icon.is-small.is-left [:i.mdi.mdi-24px.mdi-account-circle]]]]
-       [:div.field [:p.control.has-icons-left
-                    [:input.input {:placeholder "Password" :type :password :name :pass}]
-                    [:span.icon.is-small.is-left [:i.mdi.mdi-24px.mdi-lock]]]]
-       [:div.field [:p.control [:button.button {:type "submit"} "Login!"]]]
-       ]]]]])
-
 (defroutes auth-routes
            (POST "/login" [uid pass :as {session :session}]
              (handle-login uid pass session))
            (GET "/login" []
-             (page-templates/login-page login-structure)))
+             (page-templates/login-page
+               [:div#main-content.tile.is-9.is-vertical
+                [:div.tile.is-parent
+                 [:div.content.notification.tile.is-child.is-primary
+                  [:p.title "Log in om content aan te passen"]
+                  [:div.input-group.panel-body
+                   [:form.form-signin {:action "/login" :method "POST"}
+                    (anti-forgery-field)
+                    [:div.field [:p.control.has-icons-left
+                                 [:input.input {:placeholder "User ID" :type :text :name :uid}]
+                                 [:span.icon.is-small.is-left [:i.mdi.mdi-24px.mdi-account-circle]]]]
+                    [:div.field [:p.control.has-icons-left
+                                 [:input.input {:placeholder "Password" :type :password :name :pass}]
+                                 [:span.icon.is-small.is-left [:i.mdi.mdi-24px.mdi-lock]]]]
+                    [:div.field [:p.control [:button.button {:type "submit"} "Login!"]]]
+                    ]]]]])))
